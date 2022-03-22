2d69c73624f3b69539420f0f45e4e4ce77a65981
A Gordeev <nonameforman1@yandex.ru>
2022-03-22 06:51:33 +0300

    add destruct, catch; fix broken pic with load

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({card, onCardClick}) => {
     function handleClick() {
-        props.onCardClick(props.card);
+        onCardClick(card);
     }  
 
     return (
         <li className="element">
-            <button onClick={handleClick} className="element__pic-button" type="button" aria-label="Открыть изображение"><img className="element__pic" src={props.card.link} alt={props.card.name}/></button>
+            <button onClick={handleClick} className="element__pic-button" type="button" aria-label="Открыть изображение"><img className="element__pic" src={card.link} alt={card.name}/></button>
             <button className="element__delete-button" type="button" aria-label="Удалить"></button>
             <div className="element__describtion">
-                <h2 className="element__name">{props.card.name}</h2>
+                <h2 className="element__name">{card.name}</h2>
                 <div className="element__like-place">
                     <button className="element__button" type="button" aria-label="Нравится"></button>
-                    <p className="element__like-counter">{props.card.likes.lenght}</p>
+                    <p className="element__like-counter">{card.likes.lenght}</p>
                 </div>
             </div>
         </li>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-const PopupWithForm = (props) => {
-    if(props.isOpen) {
+const PopupWithForm = ({isOpen, onClose, name, title, children}) => {
+    if(isOpen) {
         document.addEventListener("keydown", (evt) => {
             if (evt.key === "Escape") {
-                props.onClose();
+                onClose();
             }
         });
     }
+
     return (
-        <div className={`popup ${props.isOpen && "popup_opened"}`} id={`popup_${props.name}`}>
+        <div className={`popup ${isOpen && "popup_opened"}`} id={`popup_${name}`}>
             <div className="popup__container">
                 <h2 className="popup__title">
-                    {props.title}
+                    {title}
                 </h2>
-                <form className="popup__form" id={`popup__form_${props.name}`} name={props.name}>
-                    {props.children}
+                <form className="popup__form" id={`popup__form_${name}`} name={name}>
+                    {children}
                 </form>
             </div>
-            <button onClick={props.onClose} className="popup__button-close button-hover" type="button" aria-label="Закрыть"></button>
-            <div onClick={props.onClose} className="popup__overlay"></div>
+            <button onClick={onClose} className="popup__button-close button-hover" type="button" aria-label="Закрыть"></button>
+            <div onClick={onClose} className="popup__overlay"></div>
         </div>
     );
 }
